37029f1951c138ceaec5b75715170cd8b8c4f095
BenjaminDeguerre <benjamin.deguerre@insa-rouen.fr>
2018-08-05 11:14:18 +0200

    formatting

--- src/main.cpp
+++ src/main.cpp
@@ -6,23 +6,20 @@
 
 #include "JPEGDecoder.hpp"
 
-int main(int argc, char const *argv[])
-{
-    if (argc == 2)
-    {
-        JPEGDecoder decoder;
-        cv::Mat image;
+int main(int argc, char const *argv[]) {
+  if (argc == 2) {
+    JPEGDecoder decoder;
+    cv::Mat image;
+    std::string file_name = argv[1];
 
-        image = decoder.Decode(argv[1], 1);
+    image = decoder.Decode(file_name, 1);
 
-        cv::namedWindow("Decoded image.", cv::WINDOW_AUTOSIZE);
-        cv::imshow("Decoded image.", image);
-        cv::waitKey(0);
-    }
-    else
-    {
-        std::cout << "Please enter one image to decode." << std::endl;
-    }
+    cv::namedWindow("Decoded image.", cv::WINDOW_AUTOSIZE);
+    cv::imshow("Decoded image.", image);
+    cv::waitKey(0);
+  } else {
+    std::cout << "Please enter one image to decode." << std::endl;
+  }
 
-    return 0;
+  return 0;
 }
