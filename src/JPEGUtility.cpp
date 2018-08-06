9758c3c943c35d785a8be7f15f508bf10b40536d
benjamin deguerre <benjamin.deguerre@actemium.com>
2018-08-06 10:58:12 +0200

    tessting the functions in utility

--- src/JPEGUtility.cpp
+++ src/JPEGUtility.cpp
@@ -6,24 +6,40 @@
 /**
  * \fn unsigned char NextBit()
  * \brief Returns the next bit in the stream.
+ *
+ * This function extract the bit at the specified position in the stream. When
+ * the bit_index value reach 0, the bit_index is set to 8 and the index is
+ * incremented by 1. If a value outside of [1 ; 8] is provided for the
+ * bit_index, the function will throw an out_of_range error.
+ *
+ * If a 0xFF byte if found in the stream, if the following byte is 0x00, the bit
+ * is extracted, else an error is thrown. If 0xFF00 is encountered, when
+ * accessing the lowest bit in the 0xFF, the index will be incremented by 2
+ * instead of 1.
+ *
+ * \param[in] file_content Pointer to the unsigned char to retrieve the bit
+ * from. \param[in, out] index Pointer containing the offset for the pointer to
+ * the byte to extract the bit from. \param[in, out] bit_index The index of the
+ * bit to extract. A value of 8 extract the highest bit in the byte, a value of
+ * 1 extract the lowest.
  */
 unsigned char NextBit(unsigned char *file_content, unsigned int *index,
                       unsigned char *bit_index) {
   unsigned char current_byte = file_content[*index], bit;
-
-  if (*bit_index == 0) {
-    *bit_index = 8;
+  if (*bit_index < 1 || *bit_index > 8) {
+    throw std::out_of_range(
+        "The index provided for the bit is out of range with a value of " +
+        *bit_index);
+  }
+  if (*bit_index == 8) {
     if (current_byte == 0xFF) {
       if (!(file_content[*index + 1] == 0x00)) {
         if (file_content[*index + 1] == DEFINE_NUMBER_OF_LINE) {
+          throw std::runtime_error("DNL byte found");
         } else {
           throw std::runtime_error(
               "Error while parsing the file, DNL byte expected but something "
               "else found");
-        }
-      } else {
-        if ((*bit_index - 1) == 0) {
-          *index += 1;
         }
       }
     }
@@ -34,7 +50,12 @@ unsigned char NextBit(unsigned char *file_content, unsigned int *index,
   *bit_index -= 1;
 
   if (*bit_index == 0) {
-    *index += 1;
+    if (current_byte == 0xFF) {
+      *index += 2;
+    } else {
+      *index += 1;
+    }
+    *bit_index = 8;
   }
   return bit;
 }
---
+++ test/test_JPEGUtility.cpp
@@ -0,0 +1,121 @@
+#include "JPEGUtility.hpp"
+#include "gtest/gtest.h"
+
+class UtilityTest : public ::testing::Test {
+ protected:
+  virtual void SetUp() {}
+
+  virtual void TearDown() {}
+};
+
+TEST_F(UtilityTest, gettingNextBit) {
+  unsigned char bit, file_content, bit_index;
+  unsigned int index;
+
+  file_content = 0x93;
+  bit_index = 8;
+  index = 0;
+
+  bit = NextBit(&file_content, &index, &bit_index);
+  ASSERT_EQ(bit, 1);
+  ASSERT_EQ(bit_index, 7);
+  ASSERT_EQ(index, 0);
+
+  bit = NextBit(&file_content, &index, &bit_index);
+  ASSERT_EQ(bit, 0);
+  ASSERT_EQ(bit_index, 6);
+  ASSERT_EQ(index, 0);
+
+  bit = NextBit(&file_content, &index, &bit_index);
+  ASSERT_EQ(bit, 0);
+  ASSERT_EQ(bit_index, 5);
+  ASSERT_EQ(index, 0);
+
+  bit = NextBit(&file_content, &index, &bit_index);
+  ASSERT_EQ(bit, 1);
+  ASSERT_EQ(bit_index, 4);
+  ASSERT_EQ(index, 0);
+
+  bit = NextBit(&file_content, &index, &bit_index);
+  ASSERT_EQ(bit, 0);
+  ASSERT_EQ(bit_index, 3);
+  ASSERT_EQ(index, 0);
+
+  bit = NextBit(&file_content, &index, &bit_index);
+  ASSERT_EQ(bit, 0);
+  ASSERT_EQ(bit_index, 2);
+  ASSERT_EQ(index, 0);
+
+  bit = NextBit(&file_content, &index, &bit_index);
+  ASSERT_EQ(bit, 1);
+  ASSERT_EQ(bit_index, 1);
+  ASSERT_EQ(index, 0);
+
+  bit = NextBit(&file_content, &index, &bit_index);
+  ASSERT_EQ(bit, 1);
+  ASSERT_EQ(bit_index, 8);
+  ASSERT_EQ(index, 1);
+}
+
+TEST_F(UtilityTest, incrementOnFF00) {
+  unsigned char bit, file_content[3] = {0xFF, 0x00, 0x23}, bit_index = 8;
+  unsigned int index = 0;
+
+  bit = NextBit(file_content, &index, &bit_index);
+  ASSERT_EQ(bit, 1);
+  ASSERT_EQ(bit_index, 7);
+  ASSERT_EQ(index, 0);
+
+  bit = NextBit(file_content, &index, &bit_index);
+  ASSERT_EQ(bit, 1);
+  ASSERT_EQ(bit_index, 6);
+  ASSERT_EQ(index, 0);
+
+  bit = NextBit(file_content, &index, &bit_index);
+  ASSERT_EQ(bit, 1);
+  ASSERT_EQ(bit_index, 5);
+  ASSERT_EQ(index, 0);
+
+  bit = NextBit(file_content, &index, &bit_index);
+  ASSERT_EQ(bit, 1);
+  ASSERT_EQ(bit_index, 4);
+  ASSERT_EQ(index, 0);
+
+  bit = NextBit(file_content, &index, &bit_index);
+  ASSERT_EQ(bit, 1);
+  ASSERT_EQ(bit_index, 3);
+  ASSERT_EQ(index, 0);
+
+  bit = NextBit(file_content, &index, &bit_index);
+  ASSERT_EQ(bit, 1);
+  ASSERT_EQ(bit_index, 2);
+  ASSERT_EQ(index, 0);
+
+  bit = NextBit(file_content, &index, &bit_index);
+  ASSERT_EQ(bit, 1);
+  ASSERT_EQ(bit_index, 1);
+  ASSERT_EQ(index, 0);
+
+  bit = NextBit(file_content, &index, &bit_index);
+  ASSERT_EQ(bit, 1);
+  ASSERT_EQ(bit_index, 8);
+  ASSERT_EQ(index, 2);
+
+  bit = NextBit(file_content, &index, &bit_index);
+  ASSERT_EQ(bit, 0);
+  ASSERT_EQ(bit_index, 7);
+  ASSERT_EQ(index, 2);
+}
+
+TEST_F(UtilityTest, testThrow) {
+  unsigned char bit, file_content[3] = {0xFF, 0x34, 0x65}, bit_index = 0;
+  unsigned int index = 0;
+
+  EXPECT_THROW(NextBit(file_content, &index, &bit_index), std::out_of_range);
+
+  bit_index = 9;
+  EXPECT_THROW(NextBit(file_content, &index, &bit_index), std::out_of_range);
+
+  bit_index = 8;
+  EXPECT_THROW(NextBit(file_content, &index, &bit_index), std::runtime_error);
+}
