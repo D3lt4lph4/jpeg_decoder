ea310965e3e481ae21d840b0c4f98631a60180ea
benjamin deguerre <benjamin.deguerre@actemium.com>
2018-10-18 18:05:59 +0200

    Removing opencv from the utility files

--- include/JPEGUtility.hpp
+++ include/JPEGUtility.hpp
@@ -3,9 +3,9 @@
 
 unsigned char NextBit(unsigned char *file_content, unsigned int *index,
                       unsigned char *bit_index);
-void IDCT(int *new_block, unsigned int component_number);
+void IDCT(int *new_block);
 void YCbCrToBGR(int *new_block);
-void FastIDCT(int *new_block, unsigned int component_number);
-static void FastIDCT1(int *x, int *y, int ps, int half);
+void FastIDCT(int *new_block);
+void FastIDCT1(int *x, int *y, int ps, int half);
 
 #endif
