d628cc68205955eae8c065e547864d92f9922cd7
benjamin deguerre <benjamin.deguerre@actemium.com>
2018-11-13 18:09:20 +0100

    setting the release mode correctly

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class CMakeBuild(build_ext):
         cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
                       '-DPYTHON_EXECUTABLE=' + sys.executable]
 
-        cfg = 'Debug' if self.debug else 'Release'
+        cfg = 'Debug' if self.debug else 'RELEASE'
         build_args = ['--config', cfg]
 
         if platform.system() == "Windows":
