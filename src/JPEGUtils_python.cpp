7684f0b65c92310e2e27fca7e847bb38bbab773e
benjamin deguerre <benjamin.deguerre@actemium.com>
2018-11-13 18:09:53 +0100

    returning numpy array without the copy

--- src/JPEGUtils_python.cpp
+++ src/JPEGUtils_python.cpp
@@ -2,6 +2,7 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <pybind11/stl_bind.h>
+#include <pybind11/numpy.h>
 
 #include "JPEGUtils.hpp"
 
@@ -9,11 +10,12 @@ PYBIND11_MAKE_OPAQUE(std::vector<int>);
 
 namespace py = pybind11;
 
+
 void initializeJPEGUtils(py::module &module) {
   py::module sub_module =
       module.def_submodule("utils", "The submodule of jpeg_decoder.");
 
-  py::bind_vector<std::vector<int>>(module, "VectorInt");
+  py::bind_vector<std::vector<int>>(module, "intVector");
 
   py::class_<JPEGImage>(sub_module, "JPEGImage")
       .def(py::init<>())
@@ -24,5 +26,8 @@ void initializeJPEGUtils(py::module &module) {
       .def("set_real_shape", &JPEGImage::SetRealShape)
       .def("rescale_to_real_size", &JPEGImage::RescaleToRealSize)
       .def("at", &JPEGImage::at, py::return_value_policy::reference)
-      .def("GetData", &JPEGImage::GetData, py::return_value_policy::reference);
+      .def("get_data", [](JPEGImage &image, int component) {
+          auto data = &image.GetData(component);
+          return py::array(data->size(), data->data());
+      });
 }
