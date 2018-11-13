6d07ee9d2fa1c6fdf3a6a5096b9da6ccddfc041f
benjamin deguerre <benjamin.deguerre@actemium.com>
2018-11-13 18:09:38 +0100

    format

--- src/JPEGDecoder_python.cpp
+++ src/JPEGDecoder_python.cpp
@@ -1,20 +1,23 @@
-#include "JPEGDecoder.hpp"
-#include "JPEGUtils.hpp"
 #include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <pybind11/stl_bind.h>
+#include "JPEGDecoder.hpp"
+#include "JPEGUtils.hpp"
 
 namespace py = pybind11;
 
 void initializeJPEGDecoder(py::module &module) {
-  py::module sub_module =
-      module.def_submodule("decoder", "The submodule of jpeg_decoder.");
+  py::module submodule = module.def_submodule("decoder");
 
-  py::class_<JPEGDecoder>(sub_module, "JPEGDecoder")
+  py::class_<JPEGDecoder>(submodule, "JPEGDecoder")
       .def(py::init<>())
       .def(py::init<const unsigned char>())
-      .def("decode_file", &JPEGDecoder::DecodeFile, py::return_value_policy::reference)
+      .def("decode_file", &JPEGDecoder::DecodeFile,
+           py::return_value_policy::reference)
       .def("get_image_size_x", &JPEGDecoder::getImageSizeX)
-      .def("get_image_size_y", &JPEGDecoder::getImageSizeY);
+      .def("get_image_size_y", &JPEGDecoder::getImageSizeY)
+      .def("get_block_per_line", &JPEGDecoder::getBlockPerLine)
+      .def("get_block_per_column", &JPEGDecoder::getBlockPerColumn)
+      .def("get_channels", &JPEGDecoder::getChannels);
 }
