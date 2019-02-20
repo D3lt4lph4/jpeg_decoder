360421f08052651966438eabf199e88a2552ba1b
BenjaminDeguerre <benjamin.deguerre@insa-rouen.fr>
2019-02-20 18:10:55 +0100

    adding script to test the time

--- python_scripts/test_python.py
+++
@@ -1,7 +0,0 @@
-from jpegdecoder import decoder
-import sys
-dec = decoder.JPEGDecoder()
-img = dec.decode_file("/data/datasets/imagenet/ILSVRC_2012_224_224/validation/n01440764/ILSVRC2012_val_00000293.JPEG", 2)
-img.get_data(0)
-del dec
-img.get_data(0)
